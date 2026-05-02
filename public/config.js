65d031eccc5c7429898843978f73dc2bb67675e9
Tran Dai Duong <duongtd@bglobalcorp.com>
2026-05-02 16:44:25 +0700

    update docker-compose config

---
+++ public/config.js
@@ -0,0 +1,3 @@
+// Runtime configuration - will be replaced by docker-entrypoint.sh at container startup
+// This file is a placeholder; the actual values are injected from environment variables
+window.ENV_CONFIG = {};
